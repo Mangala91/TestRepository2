f20bf0571ebd352e230cc1e97ccd0d01923b43b5
Mangala Hiremath <Mangalahiremath@gmail.com>
2021-03-26 18:58:24 +0530

    Adding some new files

---
+++ Project5/src/Compare.java
@@ -0,0 +1,11 @@
+
+public class Compare {
+
+	public static void main(String[] args) {
+		String a = new String ("JAVA");
+		String b = new String ("JAVA");
+		System.out.println(a. equals(b));
+
+	}
+
+}
